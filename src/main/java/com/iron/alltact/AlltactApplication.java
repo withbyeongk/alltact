a60eb8d9bb23e107496b9ac1f88ba4551319c6df
withbyeongk <withbyeongk@gmail.com>
2024-01-25 00:29:51 +0900

    Initial commit

---
+++ src/main/java/com/iron/alltact/AlltactApplication.java
@@ -0,0 +1,13 @@
+package com.iron.alltact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlltactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlltactApplication.class, args);
+	}
+
+}
